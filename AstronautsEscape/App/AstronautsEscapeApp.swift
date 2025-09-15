b7fe284d7f2709e83e1e6cf51fc88be58f25bd3d
SBabchukTM <babchuk.serhii@trident-media.agency>
2025-09-15 10:22:12 +0300

    init

---
+++ AstronautsEscape/App/AstronautsEscapeApp.swift
@@ -0,0 +1,17 @@
+//
+//  AstronautsEscapeApp.swift
+//  AstronautsEscape
+//
+//  Created by Serhii Babchuk on 15.09.2025.
+//
+
+import SwiftUI
+
+@main
+struct AstronautsEscapeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            AELaunchView()
+        }
+    }
+}
